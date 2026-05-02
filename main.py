4b6979a1b84a6e602147fdf590e8a179be40c481
PJMSCode <pjames.csec@gmail.com>
2026-05-01 23:14:39 -0500

    feat: Now generated alerts from brute force alerts are generated

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from engine.loader import load_all_logs
+from detections.brute_force import detect_brute_force
 
 
 def main() -> None:
     logs = load_all_logs()
 
+    '''
     for log_type, records in logs.items():
         print(f"{log_type}: {len(records)} records loaded")
+    '''
 
+
+    brute_force_alerts = detect_brute_force(logs["auth"])
+
+    print("Generated Alerts:")
+    for alert in brute_force_alerts:
+        print(alert)
 
 if __name__ == "__main__":
     main()
